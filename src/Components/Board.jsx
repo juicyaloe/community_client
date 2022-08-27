1a29faedc4128e6b703c6034c9f34e196c9a7db9
als8921 <als8921@naver.com>
2022-08-27 21:22:06 +0900

    MainScreen

---
+++ src/Components/Board.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Board.css";
+
+function AAA() {
+  return (
+    <div>
+      <div class="box"></div>
+      <div class="box2">
+        <text class="mainText">게시판</text>
+      </div>
+    </div>
+  );
+}
+
+export default AAA;
