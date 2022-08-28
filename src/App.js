9e451e01049dd979620d3515c6eacf54d1b6814b
juicyaloe <mink4661@snu.ac.kr>
2022-08-29 04:08:50 +0900

    Revert "Merge pull request #2 from juicyaloe/minc_220827"
    This reverts commit bdb4683f86c78b9c9a33dad17bd13663c78b42f3, reversing
    changes made to 4c6cf952b93929fac0f967cd0976be29d546e565.

--- src/App.js
+++ src/App.js
@@ -1,76 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import AAA from "./Components/Board";
-import { createStore } from "redux";
-import { Provider, useSelector, useDispatch, connect } from "react-redux";
-
-function reducer(currentState, action) {
-  if (currentState === undefined) {
-    return {
-      data: [],
-      number: 123123,
-    };
-  }
-  const newState = { ...currentState };
-
-  if (action.type === "PLUS") {
-    newState.number++;
-    Post();
-  }
-
-  if (action.type === "GET") {
-    newState.data = GetURL();
-  }
-  return newState;
-}
-
-async function GetJson() {
-  var data;
-  await fetch("file.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(
-      function (json) {
-        data = json;
-      }.bind(this)
-    );
-  console.log("AAAAA", data);
-  return data;
-}
-function Post() {
-  fetch("http://13.124.202.172/api/", {
-    method: "POST",
-    body: JSON.stringify({
-      title: "Test",
-      body: "I am testing!",
-      userId: 1,
-    }),
-  }).then((response) => console.log(response));
-}
-
-async function GetURL() {
-  var data;
-  await fetch("http://13.124.202.172/api/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(
-      function (json) {
-        data = json;
-      }.bind(this)
-    );
-  return data;
-}
-
-const store = createStore(reducer);
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Provider store={store}>
-        <AAA />
-      </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
