7b68a3720d77beaf8ab134544e6ab4a2cc7c34a4
als8921 <als8921@naver.com>
2022-08-29 15:21:13 +0900

    구경

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -1,37 +1,43 @@
 /************************************************
  * 메인 중앙 파트 컴포넌트 관련 정의
- * 
- * 
-*************************************************/
+ *
+ *
+ *************************************************/
 
 import React, { useState, useEffect } from "react";
 import BoardComp from "../components/BoardComp";
-import {getWriting, postWriting} from "../funcs/apis";
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { getWriting, postWriting } from "../funcs/apis";
+import { Provider, useSelector, useDispatch } from "react-redux";
 
 // 이하 컴포넌트의 복잡한 과정을 처리
 function Board() {
+  var [writngList, writngListFunc] = useState([]);
 
-    var [writngList, writngListFunc] = useState([]);
+  // 처음 1회만 실행
+  useEffect(function () {
+    loadWriting();
+  }, []);
 
-    // 처음 1회만 실행
-    useEffect(function() {
-        loadWriting();
-    }, []);
+  async function loadWriting() {
+    let data = await getWriting();
 
-    async function loadWriting() {
-        let data = await getWriting();
+    var listTag = [];
+    for (var i = 0; i < data.length; i++) {
+      var li = data[i];
+      listTag.push(
+        <div key={li.id}>
+          <strong>{li.title}</strong>
+          <br />
+          {li.content}
+          <br />
+          <br />
+        </div>
+      );
+    }
+    writngListFunc(listTag);
+  }
 
-        var listTag = [];
-        for(var i = 0; i<data.length; i++) {
-            var li = data[i];
-            listTag.push(<div key={li.id}><strong>{li.title}</strong><br/>{li.content}<br/><br/></div>)
-        }
-        writngListFunc(listTag);
-    } 
-
-    return <BoardComp writinglist={writngList}
-    ></BoardComp>
+  return <BoardComp writinglist={writngList}></BoardComp>;
 }
 
 export default Board;
