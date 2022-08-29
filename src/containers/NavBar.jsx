97ec35364591128cdfa624c1b8050e51063d17b1
als8921 <als8921@naver.com>
2022-08-29 15:01:37 +0900

    민재의 코드를 받아 다시 시작해보려함

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,22 +1,20 @@
 /************************************************
  * 상단 바 관련 컴포넌트 정의
- * 
- * 
-*************************************************/
+ *
+ *
+ *************************************************/
 
 import React, { useState, useEffect } from "react";
 import NavBarComp from "../components/NavBarComp";
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { Provider, useSelector, useDispatch } from "react-redux";
 
 // 이하 컴포넌트의 복잡한 과정을 처리
 function NavBar() {
+  // 처음 1회만 실행
 
-    // 처음 1회만 실행
-    useEffect(function() {
-    }, []);
+  useEffect(function () {}, []);
 
-    return <NavBarComp
-    ></NavBarComp>
+  return <NavBarComp></NavBarComp>;
 }
 
 export default NavBar;
