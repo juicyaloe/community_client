beb657449541381d6db0405109b09749fe699992
juicyaloe <mink4661@snu.ac.kr>
2022-08-31 01:47:55 +0900

    postwriting 함수 완성

--- src/components/BoardComp.jsx
+++ src/components/BoardComp.jsx
@@ -6,7 +6,8 @@
 
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css'
-import getWriting, {postWriting} from "../funcs/apis";
+import {getWriting, postWriting} from "../funcs/apis";
+import { TOKEN } from "../funcs/TOKEN";
 
 // 넘겨받은 정보를 show 하는 정도로만 구현
 function BoardComp(props) {
