9788710f2b2fb0feb1aa565f2c98857e4158f16c
als8921 <als8921@naver.com>
2022-09-01 23:58:51 +0900

    폼 분리 구현중

--- src/containers/WritingBoard.jsx
+++ src/containers/WritingBoard.jsx
@@ -1,52 +1,49 @@
 import React, { useEffect, useState } from "react";
-import {Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import WritingBoardComp from "../components/WritingBoardComp";
 
 import { postWriting, getPost } from "../funcs/apis";
-import moment from "moment";
 
 function WritingBoard(props) {
+  var [data, dataFunc] = useState();
 
-    var [data, dataFunc] = useState();
+  const url = useLocation();
+  const urlList = url.pathname.split("/");
+  const id = urlList[urlList.length - 2];
 
-    const url = useLocation()
-    const urlList = url.pathname.split("/")
-    const id = urlList[urlList.length - 2];
+  // writing/:id 에서 id 얻어오기
 
-    // writing/:id 에서 id 얻어오기
+  useEffect(
+    function () {
+      showWriting(id);
+    },
+    [id]
+  );
 
-    useEffect(function() {
-        showWriting(id);
-    }, [id]);
+  async function showWriting(id) {
+    let response = await getPost(id);
 
-    async function showWriting(id) {
-        let response = await getPost(id);
-
-        if (response.status === 200) {
-            response.json().then(
-                function(data) {
-
-                    let content = 
-                        <div>
-                            <h2>글 제목: {data.title}</h2>
-                            <p>글 내용: {data.content}</p>
-                            <p>게시판 종류: {data.board}</p>
-                            <p>글 쓴 시각: {data.inittime}</p>
-                            <Link to="/board/all/">
-                                <button>뒤로가기</button>
-                            </Link>
-                        </div>
-                    dataFunc(content);
-                }
-            )
-        } else {
-            console.log("유효하지 않은 id값")
-        }
-    
-        
+    if (response.status === 200) {
+      response.json().then(function (data) {
+        let content = (
+          <div>
+            <h2>글 제목: {data.title}</h2>
+            <p>글 내용: {data.content}</p>
+            <p>게시판 종류: {data.board}</p>
+            <p>글 쓴 시각: {data.inittime}</p>
+            <Link to="/board/all/">
+              <button>뒤로가기</button>
+            </Link>
+          </div>
+        );
+        dataFunc(content);
+      });
+    } else {
+      console.log("유효하지 않은 id값");
     }
+  }
 
-    return <WritingBoardComp data={data}></WritingBoardComp>
+  return <WritingBoardComp data={data}></WritingBoardComp>;
 }
 
-export default WritingBoard
+export default WritingBoard;
